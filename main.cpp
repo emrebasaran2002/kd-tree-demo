f2e8c5062a044bdbedc57e16fd5e48067d348afa
emrebasaran2002 <memrebasaran@gmail.com>
2024-08-26 18:42:38 -0400

    Wrote some tests

--- main.cpp
+++ main.cpp
@@ -3,35 +3,68 @@
 #include "point.hpp"
 #include <vector>
 #include <iostream>
+#include <cassert>
+#include <algorithm>
 
-void ctor_dtor() {
+struct Test final {
+    std::string name;
+    void (*execute)(void);
+
+    Test(const char* name, void (*execute)(void)): name{name}, execute{execute} {}
+};
+
+// Helper for verifying the contents of actual.
+// Precondition: expected has no duplicates.
+void assert_same_set(const std::vector<point_t>& actual, const std::vector<point_t>& expected) {
+    assert(actual.size() == expected.size());
+    for (point_t p : expected) {
+        assert(std::find(actual.cbegin(), actual.cend(), p) != actual.cend());
+    }
+}
+
+void test_empty_kdtree(void) {
     rect_t rect {0, 0, 100, 100};
-    KdTree kd1 {rect, std::vector<point_t>{}};
-    KdTree kd2 {rect, std::vector<point_t>{point_t{1, 2}}};
-    KdTree kd3 {rect, std::vector<point_t>{point_t{1, 2}, point_t{3, 4}}};
-    KdTree kd4 {rect, std::vector<point_t>{point_t{1, 2}, point_t{3, 4}, point_t{5, 3}, point_t{10, 10}}};
+    KdTree kd {rect, std::vector<point_t>{}};
+
+    assert(!kd.contains(point_t{4, 5}));
+    assert(!kd.contains(point_t{-20, -5}));
+    assert(!kd.contains(point_t{50, 102}));
+    assert(!kd.contains(point_t{60, 75}));
+
+    assert(kd.rangeSearch(rect_t{-5, -5, 45, 45}).size() == 0);
+    assert(kd.rangeSearch(rect_t{0, 0, 3, 3}).size() == 0);
+    assert(kd.rangeSearch(rect_t{50, 50, 60, 100}).size() == 0);
+    assert(kd.rangeSearch(rect_t{120, 120, 135, 150}).size() == 0);
+}
+
+void test_single_point_kdtree(void) {
+    rect_t rect {0, 0, 10, 10};
+    KdTree kd {rect, std::vector<point_t>{point_t{5, 6}}};
+
+    assert(!kd.contains(point_t{3, 9}));
+    assert(!kd.contains(point_t{11, 5}));
+    assert(!kd.contains(point_t{8, -1}));
+
+    assert(kd.contains(point_t{5, 6}));
+
+    assert(kd.rangeSearch(rect_t{3, 3, 3, 3}).size() == 0);
+    assert(kd.rangeSearch(rect_t{1, 0, 5, 6}).size() == 0);
+    assert(kd.rangeSearch(rect_t{-1, -1, 4, 4}).size() == 0);
+
+    assert_same_set(kd.rangeSearch(rect_t{5, 6, 1, 1}), std::vector<point_t>{point_t{5, 6}});
+    assert_same_set(kd.rangeSearch(rect_t{2, 2, 6, 6}), std::vector<point_t>{point_t{5, 6}});
+    assert_same_set(kd.rangeSearch(rect_t{5, 5, 4, 4}), std::vector<point_t>{point_t{5, 6}});
 }
 
 int main(void) {
-    rect_t rect {0, 0, 10, 10};
-    KdTree kd {rect, std::vector<point_t>{point_t{1, 2}, point_t{3, 4}, point_t{5, 3}, point_t{9, 9}}};
+    std::vector<Test> tests;
+    tests.emplace_back("test_empty_kdtree", &test_empty_kdtree);
+    tests.emplace_back("test_single_point_kdtree", &test_single_point_kdtree);
 
-    std::cout << kd.contains(point_t{1, 2}) << std::endl; // 1
-    std::cout << kd.contains(point_t{5, 3}) << std::endl; // 1
-    std::cout << kd.contains(point_t{4, 3}) << std::endl; // 0
-    std::cout << kd.contains(point_t{2, 2}) << std::endl; // 0
-
-    // 1,2 and 3,4
-    for (point_t p : kd.rangeSearch(rect_t {0, 0, 5, 5})) {
-        std::cout << p.x << ',' << p.y << ' ';
+    for (const Test& test : tests) {
+        std::cout << "Running: " << test.name << std::endl;
+        test.execute();
     }
-    std::cout << std::endl;
-
-    // 1,2 and 5,3
-    for (point_t p : kd.rangeSearch(rect_t {1, 1, 5, 3})) {
-        std::cout << p.x << ',' << p.y << ' ';
-    }
-    std::cout << std::endl;
-
+    std::cout << "All tests pass!" << std::endl;
     return 0;
 }
